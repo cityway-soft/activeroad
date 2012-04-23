dd8c4996686f0ec56ce6aa5932b6b006ceb50d62
Alban Peignier <alban@tryphon.eu>
2012-04-23 06:49:43 +0200

    reset_column_information in AddKindToPhysicalRoads migration

--- db/migrate/20120419093427_add_kind_to_physical_roads.rb
+++ db/migrate/20120419093427_add_kind_to_physical_roads.rb
@@ -1,7 +1,10 @@
 class AddKindToPhysicalRoads < ActiveRecord::Migration
   def change
     add_column :physical_roads, :kind, :string
+
+    ActiveRoad::PhysicalRoad.reset_column_information
     ActiveRoad::PhysicalRoad.update_all :kind => "road"
+
     add_index :physical_roads, :kind
   end
 end
