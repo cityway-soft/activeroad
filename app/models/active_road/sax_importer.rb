ae083f9630ffb4789fe7c7e2432c8a188fc6c59d
Luc Donnet <luc.donnet@dryade.net>
2012-11-06 14:27:52 +0100

    Refactor to use class in place of methods

--- app/models/active_road/sax_importer.rb
+++ app/models/active_road/sax_importer.rb
@@ -8,37 +8,91 @@ class ActiveRoad::SaxImporter
 
   def import
     parser.for_tag(:LogicalRoad).each do |logical_road|
-      logical_road_attributes = logical_road.attributes
-      ActiveRoad::LogicalRoad.create :name => logical_road['Name'], :objectid => logical_road_attributes['objectid']
+      LogicalRoadXml.new(logical_road).import
     end 
 
     parser.for_tag(:PhysicalRoad).each do |physical_road|
-      physical_road_attributes = physical_road.attributes
-      geometry = GeoRuby::SimpleFeatures::Geometry.from_ewkt( physical_road['Geometry'] ) 
-      tags =  physical_road_attributes['tags'].split(",")
-      kind = (tags.present? && tags.include?("rail") ) ? "rail" : "road"
-      logical_road = ActiveRoad::LogicalRoad.find_by_objectid( physical_road_attributes['logicalRoadRef'] )
-      ActiveRoad::PhysicalRoad.create(:geometry => geometry, :kind => kind, tags => physical_road_attributes['tags'], :logical_road_id => logical_road.id, :objectid => physical_road_attributes['objectid']) if (geometry && logical_road)
+      PhysicalRoadXml.new(physical_road).import      
     end
 
     parser.for_tag(:Junction).each do |junction|
-      junction_attributes = junction.attributes
-      ActiveRoad::Junction.create :objectid => junction_attributes['objectid'], :tags => junction_attributes['tags'], :geometry => junction['Geometry']
+      JunctionXml.new(junction).import      
     end
 
     parser.for_tag(:StreetNumber).each do |street_number|
-      street_number_attributes = street_number.attributes
-      geometry = GeoRuby::SimpleFeatures::Geometry.from_ewkt( street_number['Geometry'] ) 
-      # TODO : Fix location_on_road value
-      physical_road = ActiveRoad::PhysicalRoad.find_by_objectid( street_number_attributes['physicalRoadRef'] )
-      puts ActiveRoad::StreetNumber.create(:number => street_number_attributes['number'], :objectid => street_number_attributes['objectid'], :geometry => geometry, :location_on_road => 1, :physical_road_id => physical_road.id).errors.inspect
+      StreetNumberXml.new(street_number).import      
     end
     
   end
 
+  class ElementXml
+    attr_reader :xml
+    
+    def initialize(xml)
+      @xml = xml
+    end
 
-  def import_logical_road
+    def attributes
+      xml.attributes
+    end
+
+    def objectid
+      attributes['objectid']
+    end
+
+    def geometry
+      GeoRuby::SimpleFeatures::Geometry.from_ewkt( xml['Geometry'] ) 
+    end
+    
   end
 
- 
+  class LogicalRoadXml < ElementXml
+    
+    def name
+      xml['Name']
+    end   
+
+    def import
+      ActiveRoad::LogicalRoad.create :name => name, :objectid => objectid
+    end
+    
+  end
+
+  class PhysicalRoadXml < ElementXml
+      
+    def logical_road
+      ActiveRoad::LogicalRoad.find_by_objectid( attributes['logicalRoadRef'] )
+    end   
+
+    def kind
+      tags =  attributes['tags'].split(",")
+      (tags.present? && tags.include?("rail") ) ? "rail" : "road"
+    end
+    
+    def import
+      ActiveRoad::PhysicalRoad.create(:geometry => geometry, :kind => kind, :tags => attributes['tags'], :logical_road_id => logical_road.id, :objectid => objectid) if (geometry && logical_road)
+    end
+    
+  end
+
+  class JunctionXml < ElementXml
+
+    def import
+      ActiveRoad::Junction.create :objectid => objectid, :tags => attributes['tags'], :geometry => geometry
+    end
+    
+  end
+
+  class StreetNumberXml < ElementXml
+      # TODO : Fix location_on_road value
+    def physical_road 
+      ActiveRoad::PhysicalRoad.find_by_objectid( attributes['physicalRoadRef'] )
+    end
+      
+    def import
+      ActiveRoad::StreetNumber.create(:number => attributes['number'], :objectid => objectid, :geometry => geometry, :location_on_road => 1, :physical_road_id => physical_road.id)
+    end
+
+  end
+
 end
