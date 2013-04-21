6f6b3c7b16919938f9b9af380e1f4e951256cd95
Luc Donnet <luc.donnet@dryade.net>
2013-04-21 19:41:41 +0200

    Update travis configuration

---
+++ travis/before_script.sh
@@ -0,0 +1,9 @@
+createdb -U postgres template_postgis
+
+if [[ "$POSTGIS" == "2.0" ]]; then
+  psql -U postgres -d template_postgis -c "CREATE EXTENSION postgis;"
+else
+  createlang -U postgres plpgsql template_postgis
+  psql -U postgres -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
+  psql -U postgres -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
+fi
