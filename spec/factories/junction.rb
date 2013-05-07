62f584a019790f5456880fab7e6a11f029c76b78
Luc Donnet <luc.donnet@dryade.net>
2013-05-07 09:18:57 +0200

    Refactor factory for junction spec

--- spec/factories/junction.rb
+++ spec/factories/junction.rb
@@ -3,6 +3,11 @@ FactoryGirl.define do
   factory :junction, :class => ActiveRoad::Junction do
     sequence(:objectid) { |n| "junction::#{n}" }
     geometry GeoRuby::SimpleFeatures::Point.from_x_y(2.2946, 48.8580, ActiveRoad.srid)
+  end
+
+  factory :junction_with_physical_roads, :class => ActiveRoad::Junction do
+    sequence(:objectid) { |n| "junction::#{n}" }
+    geometry GeoRuby::SimpleFeatures::Point.from_x_y(2.2946, 48.8580, ActiveRoad.srid)
 
     after(:create) do |junction|
       junction.physical_roads = FactoryGirl.create_list(:physical_road, 2)
