ce43f4b5b875290ace8a57f7765cc69daa78ab27
Luc Donnet <luc.donnet@dryade.net>
2013-10-23 10:46:24 +0200

    Add for travis the install of ruby bindings for kyotocabinet

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -4,10 +4,23 @@ fi
 
 sudo apt-get update
 sudo apt-get install -qq libgeos-dev libproj-dev postgresql-9.1-postgis liblzo2-dev liblzma-dev zlib1g-dev build-essential
+
+# Se placer dans le dossier /tmp
+cd /tmp
+
+# Installer kyotocabinet
 wget http://fallabs.com/kyotocabinet/pkg/kyotocabinet-1.2.76.tar.gz
 tar xzf kyotocabinet-1.2.76.tar.gz
 cd kyotocabinet-1.2.76
 ./configure –enable-zlib –enable-lzo –enable-lzma && make
+make install
+
+# Installer les bindings ruby pour kyotocabinet
+wget http://fallabs.com/kyotocabinet/rubypkg/kyotocabinet-ruby-1.32.tar.gz
+cd kyotocabinet-ruby-1.32
+ruby extconf.rb
+make
+ruby test.rb
 make install
 
 if [[ "$POSTGIS" == "2.0" ]]; then
