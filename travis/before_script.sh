39cb31afadc1c5c3a624ac2d0d175f210a1120d7
Luc Donnet <luc.donnet@dryade.net>
2013-11-08 23:34:44 +0100

    Add gist index for postgis 2.0

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -2,6 +2,7 @@ createdb -U postgres template_postgis
 
 if [[ "$POSTGIS" == "2.0" ]]; then
   psql -U postgres -d template_postgis -c "CREATE EXTENSION postgis;"
+  psql -U postgres -d template_postgis -c "CREATE EXTENSION postgis_topology;"
 else
   createlang -U postgres plpgsql template_postgis
   psql -U postgres -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
