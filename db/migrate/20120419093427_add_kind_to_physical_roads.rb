e3609b80ab14cda4b6ba5c9c52ae0aad4e71760a
Luc Donnet <luc.donnet@dryade.net>
2013-11-15 13:45:50 +0100

    Delete lines which fails on Centos postgresql

--- db/migrate/20120419093427_add_kind_to_physical_roads.rb
+++ db/migrate/20120419093427_add_kind_to_physical_roads.rb
@@ -2,8 +2,8 @@ class AddKindToPhysicalRoads < ActiveRecord::Migration
   def change
     add_column :physical_roads, :kind, :string
 
-    ActiveRoad::PhysicalRoad.reset_column_information
-    ActiveRoad::PhysicalRoad.update_all :kind => "road"
+    #ActiveRoad::PhysicalRoad.reset_column_information
+    #ActiveRoad::PhysicalRoad.update_all :kind => "road"
 
     add_index :physical_roads, :kind
   end
