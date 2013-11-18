b16ed1f0bef90736fc667e80678adb9233d78ab3
Luc Donnet <luc.donnet@dryade.net>
2013-11-18 13:45:09 +0100

    Update specs for physical road

--- spec/models/active_road/physical_road_spec.rb
+++ spec/models/active_road/physical_road_spec.rb
@@ -14,4 +14,35 @@ describe ActiveRoad::PhysicalRoad do
     subject.should be_valid
   end
 
+  describe ".nearest_to" do 
+    let(:departure) { point(0, 0) }
+    let(:ab) { create(:physical_road, :geometry => line_string( "0.01 0.01,1 1" ) ) }
+    let(:ac) { create(:physical_road, :geometry => line_string( "-0.02 -0.02,-1 -1" ) )  }
+    let(:ad) { create(:physical_road, :geometry => line_string( "-0.011 -0.011,-1 -1" ) )  }
+    
+    it "should return physical roads in an area ordered from closest to farthest from a departure" do
+      ActiveRoad::PhysicalRoad.nearest_to(departure).should == [ab, ad]
+    end
+  end
+
+  describe "closest_to" do 
+    let(:departure) { point(0, 0) }
+    let(:ab) { create(:physical_road, :geometry => line_string( "0.01 0.01,1 1" ) ) }
+    let(:ac) { create(:physical_road, :geometry => line_string( "-0.02 -0.02,-1 -1" ) )  }
+    
+    it "should return physical roads ordered from closest to farthest from a departure" do
+      ActiveRoad::PhysicalRoad.closest_to(departure).should == [ab, ac]
+    end
+  end
+
+  describe "with_in" do 
+    let(:departure) { point(0, 0) }
+    let(:ab) { create(:physical_road, :geometry => line_string( "0.01 0.01,1 1" ) ) }
+    let(:ac) { create(:physical_road, :geometry => line_string( "-0.02 -0.02,-1 -1" ) )  }
+
+    it "should return physical road in an area" do
+      ActiveRoad::PhysicalRoad.with_in(departure, 100).should == [ab]
+    end
+  end
+
 end
