633e5a61886e122baba95a8d9f8f159f21fc6603
Luc Donnet <luc.donnet@dryade.net>
2013-11-25 10:59:30 +0100

    Fix script install for postgis 2.1 in travis

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,4 +1,4 @@
 createdb -U postgres template_postgis
 
 psql -U postgres -d template_postgis -c "CREATE EXTENSION postgis;"
-psql -U postgres -d template_postgis -f "/usr/share/postgresql/9.3/contrib/postgis-2.1/legacy_gist.sql"
+#psql -U postgres -d template_postgis -f "/usr/share/postgresql/9.3/contrib/postgis-2.1/legacy_gist.sql"
