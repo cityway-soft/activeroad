860a5c94b00c0b38cc27e8ae49b6608cf0c9b2f9
Luc Donnet <luc.donnet@dryade.net>
2013-11-25 10:04:18 +0100

    Fix travis config delete use of postgis 1.5

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,11 +1,4 @@
-if [[ "$POSTGIS" == "2.0" ]]; then
-    echo "yes" | sudo apt-add-repository ppa:ubuntugis/ubuntugis-unstable
-    sudo apt-get update
-    sudo apt-get install -qq libgeos++-dev libproj-dev postgresql-9.1-postgis-2.0 postgresql-9.1-postgis-2.0-scripts build-essential liblzo2-dev liblzma-dev zlib1g-dev libprotobuf-c0-dev
-  else
-    sudo apt-get update
-    sudo apt-get install -qq libgeos-dev libproj-dev postgresql-9.1-postgis build-essential liblzo2-dev liblzma-dev zlib1g-dev libprotobuf-c0-dev
-fi
+sudo apt-get install -qq libgeos++-dev libproj-dev postgresql-9.1-postgis-2.0 postgresql-9.1-postgis-2.0-scripts build-essential liblzo2-dev liblzma-dev zlib1g-dev libprotobuf-c0-dev
 
 # Se placer dans le dossier /tmp
 cd /tmp
--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,10 +1,4 @@
 createdb -U postgres template_postgis
 
-if [[ "$POSTGIS" == "2.0" ]]; then
-  psql -U postgres -d template_postgis -c "CREATE EXTENSION postgis;"
-  psql -U postgres -d template_postgis -f "/usr/share/postgresql/9.1/contrib/postgis-2.0/legacy_gist.sql"
-else
-  createlang -U postgres plpgsql template_postgis
-  psql -U postgres -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
-  psql -U postgres -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
-fi
+psql -U postgres -d template_postgis -c "CREATE EXTENSION postgis;"
+psql -U postgres -d template_postgis -f "/usr/share/postgresql/9.3/contrib/postgis-2.1/legacy_gist.sql"
