cd9408d1b65d0c5d49a59dc6a478cc54dcf2e998
Luc Donnet <ldonnet@cityway.fr>
2013-11-26 10:46:00 +0100

    list files for postgis script

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,4 +1,6 @@
 createdb -U postgres template_postgis
 
 psql -U postgres -d template_postgis -c "CREATE EXTENSION postgis;"
+ls -al /usr/share/postgresql/9.3/contrib
+ls -al /usr/share/postgresql/9.3/contrib/postgis-2.1
 psql -U postgres -d template_postgis -f "/usr/share/postgresql/contrib/postgis-2.1/legacy_gist.sql"
