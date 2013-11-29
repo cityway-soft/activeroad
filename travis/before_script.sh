a9fb652d13c7425465a47e6dc8e94dd26ecea566
Luc Donnet <ldonnet@cityway.fr>
2013-11-29 16:55:59 +0100

    Add postgis topology in travis before script

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,9 +1,8 @@
 createdb -U postgres template_postgis
 
 psql -U postgres -d template_postgis -c "CREATE EXTENSION postgis;"
-locate 'legacy.sql'
-ls -al /usr/share/postgresql/9.3
-ls -al /usr/share/postgresql/9.3/contrib
-ls -al /usr/share/postgresql/9.3/extension
+psql -U postgres -d template_postgis -c "CREATE EXTENSION postgis_topology;"
+
 ls -al /usr/share/postgresql/9.3/contrib/postgis-2.1
-psql -U postgres -d template_postgis -f "/usr/share/postgresql/9.3/contrib/postgis-2.1/legacy_gist.sql"
+
+#psql -U postgres -d template_postgis -f "/usr/share/postgresql/9.3/contrib/postgis-2.1/legacy_gist.sql"
