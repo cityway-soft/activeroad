7a0e4a5d702b8cfe7fd3f3a25e4173f9e555757e
Luc Donnet <ldonnet@cityway.fr>
2013-12-09 17:44:55 +0100

    Fix travis configuration and revert to postgresql 9.2 with postgis 2.1

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -3,6 +3,7 @@ createdb -U postgres template_postgis
 psql -U postgres -d template_postgis -c "CREATE EXTENSION postgis;"
 psql -U postgres -d template_postgis -c "CREATE EXTENSION postgis_topology;"
 
-ls -al /usr/share/postgresql/9.3/contrib/postgis-2.1
+ls -al /usr/share/postgresql/9.2/contrib/postgis-2.1
 
-#psql -U postgres -d template_postgis -f "/usr/share/postgresql/9.3/contrib/postgis-2.1/legacy_gist.sql"
+# Problem with postgresql 9.3 no script anymore for postgis
+psql -U postgres -d template_postgis -f "/usr/share/postgresql/9.2/contrib/postgis-2.1/legacy_gist.sql"
