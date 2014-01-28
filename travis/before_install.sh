793454abd2008df00014fc059e2497f3a2465643
Luc Donnet <ldonnet@cityway.fr>
2014-01-28 13:47:02 +0100

    Update package before install in travis configuration

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,3 +1,5 @@
+sudo apt-get update
+
 sudo apt-get install -qq libgeos++-dev libproj-dev build-essential liblzo2-dev liblzma-dev zlib1g-dev libprotobuf-c0-dev postgresql-contrib-9.3 postgresql-9.3-postgis-2.1-scripts libprotobuf-c0 zlib1g
 
 # Se placer dans le dossier /tmp
