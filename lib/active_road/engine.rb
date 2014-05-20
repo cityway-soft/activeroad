3bd7f6c8fe6da73f9e7544ecd68c8c084735f01b
Luc Donnet <ldonnet@cityway.fr>
2014-05-20 20:13:15 +0200

    Add rspec to engine

--- lib/active_road/engine.rb
+++ lib/active_road/engine.rb
@@ -3,7 +3,10 @@ module ActiveRoad
     require "active_road/migration"
 
     class Engine < ::Rails::Engine
-
+      config.generators do |g|
+        g.test_framework :rspec
+      end
+      
       initializer "active_road.factories", :after => "factory_girl.set_factory_paths" do
         FactoryGirl.definition_file_paths << File.expand_path('../../../spec/factories', __FILE__) if defined?(FactoryGirl)
       end
