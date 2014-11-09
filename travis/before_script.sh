4185c88e764660705318d6423207c77463227f0d
Luc Donnet <ldonnet@cityway.fr>
2014-11-09 21:49:13 +0100

    Fix travis spec

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,9 +1,9 @@
-createdb -U postgres template_postgis
+# createdb -U postgres template_postgis
 
-psql -U postgres -d template_postgis -c "CREATE EXTENSION postgis;"
-psql -U postgres -d template_postgis -c "CREATE EXTENSION postgis_topology;"
+# psql -U postgres -d template_postgis -c "CREATE EXTENSION postgis;"
+# psql -U postgres -d template_postgis -c "CREATE EXTENSION postgis_topology;"
 
-ls -al /usr/share/postgresql/9.2/contrib/postgis-2.1
+# ls -al /usr/share/postgresql/9.2/contrib/postgis-2.1
 
-# Problem with postgresql 9.3 no script anymore for postgis
-psql -U postgres -d template_postgis -f "/usr/share/postgresql/9.2/contrib/postgis-2.1/legacy_gist.sql"
+# # Problem with postgresql 9.3 no script anymore for postgis
+# psql -U postgres -d template_postgis -f "/usr/share/postgresql/9.2/contrib/postgis-2.1/legacy_gist.sql"
